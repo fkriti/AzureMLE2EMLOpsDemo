e322d581b3ca46501438c2b4dd2de49521b95173
Kriti <53083330+fkriti@users.noreply.github.com>
2022-10-16 15:44:59 -0700

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -4,19 +4,21 @@ az version
 az extension remove -n azure-cli-ml
 az extension remove -n ml
 
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/72468942/ml-0.0.72468942-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/72468942 --yes || {
+az extension add -n ml -y
+
+#az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/72468942/ml-0.0.72468942-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/72468942 --yes || {
 #az extension add -n ml -y  || {
-    echo "az extension add -n ml -y failed..."; exit 1;
-}
+#    echo "az extension add -n ml -y failed..."; exit 1;
+#}
 
 
 # Use defaults if not passed by workflow inputs
 
-GROUP1=${GROUP:-"rg-contoso-819prod"}
+GROUP1=${GROUP:-"kritifaujdar-rg"}
 
-LOCATION1=${LOCATION:-"eastus"}
+LOCATION1=${LOCATION:-"westus"}
 
-WORKSPACE1=${WORKSPACE:-"mlw-contoso-819prod"}
+WORKSPACE1=${WORKSPACE:-"kritifaujdar-ws"}
 
 az configure --defaults group=$GROUP1 workspace=$WORKSPACE1 location=$LOCATION1
 
